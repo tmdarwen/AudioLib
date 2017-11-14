46a85a4ab7b46d5c790a95c218dfae61a63c5ff0
Terence Darwen <tmdarwen@gmail.com>
2017-11-13 18:00:59 -0600

    Updated automation, documentation and readme, removing application specific info

--- Automation/CI/JenkinsBuildCIWindows.bat
+++
@@ -1,6 +0,0 @@
-call "%VisualStudioPath%\vcvarsall.bat" amd64
-mkdir PhaseVocoderBuilt
-cd PhaseVocoderBuilt
-cmake -G "%CMakeGeneratorString%" ../Source
-devenv PhaseVocoder.sln /Build Debug
-devenv PhaseVocoder.sln /Build Release
